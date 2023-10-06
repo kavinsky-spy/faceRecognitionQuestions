2c8e96167d0523c9593494e2a7d226cb2233c9a8
julioretkwa <julio@zoocha.com>
2023-10-06 19:39:31 -0300

    wip: fix double canvas render

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const video = document.getElementById('video')
+const video = document.getElementById('video');
+
+const once = fn => (...args) => {
+    if (!fn) return;
+    fn(...args);
+    fn = null;
+  };
 
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
     faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
     faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
     faceapi.nets.faceExpressionNet.loadFromUri('/models')
-]).then(startVideo)
+]).then(startVideo);
 
 function startVideo() {
     navigator.getUserMedia({ video: {} },
@@ -17,16 +23,16 @@ function startVideo() {
 video.addEventListener('play', () => {
     const canvas = faceapi.createCanvasFromMedia(video)
     const element = document.getElementById("video-container");
-    element.append(canvas)
-    const displaySize = { width: video.width, height: video.height }
-    faceapi.matchDimensions(canvas, displaySize)
+    once(element.append(canvas));
+    const displaySize = { width: video.width, height: video.height };
+    faceapi.matchDimensions(canvas, displaySize);
     setInterval(async () => {
-        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-        const resizedDetections = faceapi.resizeResults(detections, displaySize)
-        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-        faceapi.draw.drawDetections(canvas, resizedDetections)
-        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+        const resizedDetections = faceapi.resizeResults(detections, displaySize);
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+        faceapi.draw.drawDetections(canvas, resizedDetections);
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
         // console.log(detections[0].expressions["happy"])
         if (detections[0].expressions["happy"] >= 0.99) {
             console.log("happy")
