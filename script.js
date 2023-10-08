f812326233a5a7196b0e21b7795aa4e2440d81b4
julioretkwa <julio@zoocha.com>
2023-10-08 02:44:10 -0300

    wip: fill questions function redoing layout

--- script.js
+++ script.js
@@ -1,45 +1,125 @@
-const video = document.getElementById('video');
+const video = document.getElementById("video");
 
-const once = fn => (...args) => {
+const once =
+  (fn) =>
+  (...args) => {
     if (!fn) return;
     fn(...args);
     fn = null;
   };
 
 Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
 ]).then(startVideo);
 
 function startVideo() {
-    navigator.getUserMedia({ video: {} },
-        stream => video.srcObject = stream,
-        err => console.error(err)
-    )
+  navigator.getUserMedia(
+    { video: {} },
+    (stream) => (video.srcObject = stream),
+    (err) => console.error(err)
+  );
 }
 
-video.addEventListener('play', () => {
-    const canvas = faceapi.createCanvasFromMedia(video)
-    const element = document.getElementById("video-container");
-    once(element.append(canvas));
-    const displaySize = { width: video.width, height: video.height };
-    faceapi.matchDimensions(canvas, displaySize);
-    setInterval(async () => {
-        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
-        const resizedDetections = faceapi.resizeResults(detections, displaySize);
-        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-        faceapi.draw.drawDetections(canvas, resizedDetections);
-        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-        faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
-        // console.log(detections[0].expressions["happy"])
-        if (detections[0].expressions["happy"] >= 0.99) {
-            console.log("happy")
-        }
-        if (detections[0].expressions["angry"] >= 0.99) {
-            console.log("angry")
-        }
+video.addEventListener("play", () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  const element = document.getElementById("video-container");
+  once(element.append(canvas));
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+  setInterval(
+    async () => {
+      const detections = await faceapi
+        .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+        .withFaceLandmarks()
+        .withFaceExpressions();
+      const resizedDetections = faceapi.resizeResults(detections, displaySize);
+      canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+      faceapi.draw.drawDetections(canvas, resizedDetections);
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+      faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+      // console.log(detections[0].expressions["happy"])
+      if (detections[0].expressions["happy"] >= 0.99) {
+        console.log("happy");
+      }
+      if (detections[0].expressions["angry"] >= 0.99) {
+        console.log("angry");
+      }
+    },
+    100,
+    { once: true }
+  );
+});
 
-    }, 100, {once : true})
-})
+const question = document.querySelector(".question");
+const answers = document.querySelector(".answers");
+const spnQtd = document.querySelector(".spnQtd");
+const spnImg = document.querySelector(".spnImg");
+const textFinish = document.querySelector(".finish span");
+const content = document.querySelector(".content");
+const contentFinish = document.querySelector(".finish");
+const btnRestart = document.querySelector(".finish button");
+
+let currentIndex = 0;
+let questionsCorrect = 0;
+
+btnRestart.onclick = () => {
+  content.style.display = "flex";
+  contentFinish.style.display = "none";
+
+  currentIndex = 0;
+  questionsCorrect = 0;
+  loadQuestion();
+};
+
+function nextQuestion(e) {
+  if (e.target.getAttribute("data-correct") === "true") {
+    questionsCorrect++;
+  }
+
+  if (currentIndex < questions.length - 1) {
+    currentIndex++;
+    loadQuestion();
+  } else {
+    finish();
+  }
+}
+
+function finish() {
+  textFinish.innerHTML = `você acertou ${questionsCorrect} de ${questions.length}`;
+  content.style.display = "none";
+  contentFinish.style.display = "flex";
+}
+
+function loadQuestion() {
+  spnImg.innerHTML = "";
+  spnQtd.innerHTML = `${currentIndex + 1}/${questions.length}`;
+
+  const imgElement = document.createElement("img");
+  const item = questions[currentIndex];
+  answers.innerHTML = "";
+  question.innerHTML = item.question;
+  imgElement.src = item.img;
+
+  item.answers.forEach((answer) => {
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+    <button class="answer" data-correct="${answer.correct}">
+      ${answer.option}
+    </button>
+    `;
+
+    answers.appendChild(div);
+  });
+
+  spnImg.appendChild(imgElement);
+
+  document.querySelectorAll(".answer").forEach((item) => {
+    item.addEventListener("click", nextQuestion);
+  });
+}
+
+loadQuestion();
---
+++ src/questions.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    question: "What is the largest mammal in the world?",
+    answers: [
+      { option: "African Elephant", correct: false },
+      { option: "Blue Whale", correct: true },
+      { option: "Giraffe", correct: false },
+      { option: "Hippopotamus", correct: false },
+    ],
+    img: `https://picsum.photos/id/${Math.floor(Math.random() * 111)}/800/600`,
+  },
+  {
+    question: 'Who wrote the play "Romeo and Juliet"?',
+    answers: [
+      { option: "Charles Dickens", correct: false },
+      { option: "William Shakespeare", correct: true },
+      { option: "Jane Austen", correct: false },
+      { option: "Mark Twain", correct: false },
+    ],
+    img: `https://picsum.photos/id/${Math.floor(Math.random() * 111)}/800/600`,
+  },
+  {
+    question: "What is the chemical symbol for the element gold?",
+    answers: [
+      { option: "Go", correct: false },
+      { option: "Gd", correct: false },
+      { option: "Au", correct: true },
+      { option: "Ag", correct: false },
+    ],
+    img: `https://picsum.photos/id/${Math.floor(Math.random() * 111)}/800/600`,
+  },
+  {
+    question: "Which country is known as the Land of the Rising Sun?",
+    answers: [
+      { option: "China", correct: false },
+      { option: "South Korea", correct: false },
+      { option: "Japan", correct: true },
+      { option: "Thailand", correct: false },
+    ],
+    img: `https://picsum.photos/id/${Math.floor(Math.random() * 111)}/800/600`,
+  },
+  {
+    question: "Which famous scientist is known for the theory of relativity?",
+    answers: [
+      { option: "Isaac Newton", correct: false },
+      { option: "Albert Einstein", correct: true },
+      { option: "Galileo Galilei", correct: false },
+      { option: "Stephen Hawking", correct: false },
+    ],
+    img: `https://picsum.photos/id/${Math.floor(Math.random() * 111)}/800/600`,
+  },
+  {
+    question: "What is the largest planet in our solar system?",
+    answers: [
+      { option: "Venus", correct: false },
+      { option: "Mars", correct: false },
+      { option: "Jupiter", correct: true },
+      { option: "Saturn", correct: false },
+    ],
+    img: `https://picsum.photos/id/${Math.floor(Math.random() * 111)}/800/600`,
+  },
+];
